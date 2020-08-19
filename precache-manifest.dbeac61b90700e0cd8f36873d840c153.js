806254c1513febf7ae6de5b46a7af03fbf4347d4
Gareth Brown <beedyg@googlemail.com>
2020-08-19 15:19:19 +0100

    Updates

---
+++ precache-manifest.dbeac61b90700e0cd8f36873d840c153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9258e8d431b780ca20c269f602503f5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0df1fceba50f51d61eb5",
+    "url": "/monsters-rolodex/static/css/main.bb6ecd11.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df1fceba50f51d61eb5",
+    "url": "/monsters-rolodex/static/js/main.72894281.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
